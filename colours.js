ae83ba0c42181a426949ca003c7c211688fa3888
rossington19 <rossington@live.com>
2020-06-24 13:30:34 +0100

    Delete colours.js

--- colours.js
+++
@@ -1,8 +0,0 @@
-function defineColours(){
-	return [
-		//dragged			//rollover		//colour
-		[color(255,50,50),	color(235,50,50),	color(209,49,49)],		//red
-		[color(60,150,255),	color(0,110,255),	color(39,71,221)],		//blue
-		[color(85),			color(55),			color(35)],				//black
-	]
-}
